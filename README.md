92f7f9771b0abddf2e1f58e0d7df441bf2c9b1ec
Pradiptaa <diptaarya18@gmail.com>
2024-05-03 19:38:00 +0700

    Experiment 1.2: Understanding how it works.

---
+++ README.md
@@ -0,0 +1,7 @@
+## Pradipta Arya P. - 2206083685
+### Module 10 - Timer
+
+**1.2 Understanding How it Works**
+![](assets/module10timer2.png)
+
+Dapat dilihat dari output bahwa "hey hey" akan muncul pertama karena "hey hey" berada di fungsi main dan bukan pada fungsi async.
--- src/main.rs
+++ src/main.rs
@@ -104,6 +104,7 @@ fn main() {
         TimerFuture::new(Duration::new(2, 0)).await;
         println!("Pradipta's Computer: done!");
     });
+    println!("Pradipta's Computer: hey hey");
 
     // Drop the spawner so that our executor knows it is finished and won't
     // receive more incoming tasks to run.
